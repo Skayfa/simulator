05716306e7da937244b2bc8e6627857e891dc725
Skayfa <younesbeaila@gmail.com>
2022-06-06 11:29:40 +0200

    add tailwind

---
+++ front/components/FooterComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FooterComponent: React.FC = () => {
+  return (
+    <footer>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{" "}
+      </a>
+    </footer>
+  );
+};
+
+export default FooterComponent;
--- front/lib/graphql.ts
+++ front/lib/graphql.ts
@@ -1,5 +1,6 @@
 import { createClient } from "urql";
 
-const url = "https://simulator-six.vercel.app/graphql";
+// const url = "https://simulator-six.vercel.app/graphql";
+const url = "http:/localhost:4000/graphql";
 
 export const client = createClient({ url });
